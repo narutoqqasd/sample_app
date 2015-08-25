cd30653904df1d8f8f85c8873453c32842e27cf7
MyWay1992 <312126780@qq.com>
2015-08-25 08:43:27 +0800

    add signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   #get 'static_pages/home'
   root 'static_pages#home'
   
-  get 'static_pages/help'
+  
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  
+  get 'about' => 'static_pages#about'
 
-  get 'static_pages/contact'
+  
+  get 'contact' => 'static_pages#contact'
+
+  get 'signup'  => 'users#new'
 
   
 
